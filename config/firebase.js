d512260ae751387604c57dff8bba0fc6db5afea0
BIVA Developer <biva@example.com>
2026-05-09 18:08:18 +0100

    fixed production api urls

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccount.json");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -7,4 +12,7 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-module.exports = { admin, db };
+module.exports = {
+  admin,
+  db,
+};
