21cc7cdcd5e3f098db535e39a44a18873b7a19d0
brightinternationaldamaturu-hue <brightinternationaldamaturu@gmail.com>
2026-05-15 19:50:02 +0100

    Update data.routes.js

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -1,16 +1,37 @@
+const express = require("express");
+
+const router = express.Router();
+
+// IMPORT CONTROLLER
+const dataController =
+  require("../controllers/data.controller");
+
+
+// ============================
+// GET DATA PLANS
+// ============================
 router.get(
   "/plans/:network_id",
   dataController.getPlans
 );
 
+
+// ============================
+// BUY DATA
+// ============================
 router.post(
   "/buy",
   dataController.buyData
 );
 
+
+// ============================
+// WITHDRAW CASHBACK
+// ============================
 router.post(
   "/withdraw-cashback",
   dataController.withdrawCashback
 );
+
 
 module.exports = router;
