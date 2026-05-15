113d95492f9507d570b7254758b7509f539b17ff
brightinternationaldamaturu-hue <brightinternationaldamaturu@gmail.com>
2026-05-15 19:03:26 +0100

    Update data.controller.js

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -150,9 +150,10 @@ exports.buyData = async (req, res) => {
 
     let profit = 0;
 
-    if (originalAmount <= 300) profit = 13;
+    if (originalAmount <= 200) profit = 30;
+    else if (originalAmount <= 800) profit = 40;
     else if (originalAmount <= 1000) profit = 50;
-    else if (originalAmount <= 2000) profit = 65;
+    else if (originalAmount <= 2000) profit = 80;
     else if (originalAmount <= 3500) profit = 100;
     else if (originalAmount <= 5000) profit = 150;
     else profit = 200;
