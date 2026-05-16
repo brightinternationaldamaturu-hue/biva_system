57b5ca2c934caaceef3264bd448ec5073656e7fb
brightinternationaldamaturu-hue <brightinternationaldamaturu@gmail.com>
2026-05-16 10:59:31 +0100

    Update wallet.routes.js

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -1,9 +1,26 @@
 const express = require("express");
+
 const router = express.Router();
 
-const walletController = require("../controllers/wallet.controller");
+const walletController =
+  require("../controllers/wallet.controller");
 
-// IMPORTANT: must be function reference
-router.post("/fund", walletController.fundWallet);
+// FUND WALLET
+router.post(
+  "/fund",
+  walletController.fundWallet
+);
+
+// CREDIT WALLET
+router.post(
+  "/credit",
+  walletController.creditWallet
+);
+
+// GET WALLET
+router.get(
+  "/wallet/:uid",
+  walletController.getWallet
+);
 
 module.exports = router;
