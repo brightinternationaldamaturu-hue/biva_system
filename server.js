021a637823ac91052598e695e4a1a21557313f63
brightinternationaldamaturu-hue <brightinternationaldamaturu@gmail.com>
2026-05-21 21:05:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/data", require("./routes/data.routes"));
 app.use("/api",require("./routes/flutterwave.webhook.routes"));
 app.use("/api/electricity",electricityRoutes);
+app.use("/api", require("./routes/voucher.routes"));
 
 // VIRTUAL ACCOUNT ROUTE
 app.use(
