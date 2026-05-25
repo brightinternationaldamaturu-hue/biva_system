6551c6cd09059600a1cdc6c319aec69e72039a17
brightinternationaldamaturu-hue <brightinternationaldamaturu@gmail.com>
2026-05-25 08:48:51 -0700

    Delete services/email.service.js

--- services/email.service.js
+++
@@ -1,8 +0,0 @@
-const brevo = require('@getbrevo/brevo');
-
-const client = brevo.ApiClient.instance;
-client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
-
-const apiInstance = new brevo.TransactionalEmailsApi();
-
-module.exports = apiInstance;
