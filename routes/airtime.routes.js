f4d77d43a3d6d430b5928d3fb2ef7016ff89d823
brightinternationaldamaturu-hue <brightinternationaldamaturu@gmail.com>
2026-05-25 08:39:11 -0700

    Update airtime.routes.js

--- routes/airtime.routes.js
+++ routes/airtime.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { buyAirtime } = require("../controllers/airtime.controller");
+const { sendEmail, transactionTemplate } = require("../services/email.service");
 
 // FINAL ROUTE
 router.post("/buy-airtime", buyAirtime);
