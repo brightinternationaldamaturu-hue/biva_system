b999767bb95c347e8a4836ddaf880a415af27eaf
brightinternationaldamaturu-hue <brightinternationaldamaturu@gmail.com>
2026-06-14 09:50:30 -0700

    Update API endpoints for internet usage sync

--- services/internetUsageSync.js
+++ services/internetUsageSync.js
@@ -25,7 +25,7 @@ const activeAccounts =
 
 const recordsResponse =
   await axios.get(
-    "https://further-investigations-seconds-cake.trycloudflare.com/omada/authed-records"
+    "https://biva-omada-agent.onrender.com/omada/authed-records"
   );
 
 const records =
@@ -258,7 +258,7 @@ for(
 
       await axios.post(
 
-        "https://further-investigations-seconds-cake.trycloudflare.com/omada/disconnect-client",
+        "https://biva-omada-agent.onrender.com/omada/disconnect-client",
 
         {
 
